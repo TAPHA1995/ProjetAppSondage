a62fc282b75cf1e2af6ef488074cfb96f9cb689e
TAPHA1995 <souane91@gmail.com>
2024-01-24 18:03:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import logo from './logo.svg';
+import './App.css';
+
+import Homepage from './Pages/Pagesaffichesondage/Homepage';
+// const background = new URL("./images/hero-bg.png",import.meta.url)
+
+function App() {
+  return (
+    <div className="App">
+  <Homepage/>
+  <div>
+      {/* <img scr={background}/> */}
+   </div>
+   </div>
+  );
+}
+export default App;
